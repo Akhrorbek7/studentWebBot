455f8b76377bbd0b6872d44f5b3a193d626e65f8
Akhrorbek7 <akhrorbekabdukhamidov7@gmail.com>
2022-04-03 18:29:08 +0500

    add security

--- StudentWeb-master/demo/src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ StudentWeb-master/demo/src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
